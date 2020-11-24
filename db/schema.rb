21c302e383d45f976f2ed1d3a866835d9ccaff5c
Ubuntu <ubuntu@ip-172-31-16-17.ec2.internal>
2020-11-24 04:34:06 +0000

    Add name age to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_032852) do
+ActiveRecord::Schema.define(version: 2020_11_24_042652) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,9 @@ ActiveRecord::Schema.define(version: 2020_11_24_032852) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.integer "age"
+    t.index "\"user_id\"", name: "index_users_on_user_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
